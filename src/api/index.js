fe6d81b822e0d6f377dfaa6f9a9fad54d687a211
Joyce Lin <joycelin79@gmail.com>
2019-11-26 19:43:25 -0800

    add read and write endpoints for moisture sensor

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,47 @@
-const express = require('express')
-require('dotenv').config()
-const postgres = require('@metamodules/postgres')()
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const redis = require("@metamodules/redis")();
 
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
-postgres.query(`CREATE TABLE IF NOT EXISTS clicks (
-  id BIGSERIAL PRIMARY KEY,
-  created_at TIMESTAMP DEFAULT NOW()
-)`)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/api/count', (req, res) => {
-  postgres.query('SELECT count(*) AS count FROM clicks', (err, resp) => {
-    res.send({ count: resp.rows[0].count || 0 })
-  })
-})
+app.get("/water", (req, res) => {
+  // https://redis.io/commands/zrangebyscore
+  let args = ["series", "-inf", "+inf", "WITHSCORES"];
+  redis.ZRANGEBYSCORE(args, function(err, measurements) {
+    if (err) throw err;
 
-app.post('/api/count/increment', (req, res) => {
-  postgres.query('INSERT INTO clicks DEFAULT VALUES', (err, insert) => {
-    postgres.query('SELECT count(*) AS count FROM clicks', (err, resp) => {
-      res.send({ count: resp.rows[0].count || 0 })
-    })
-  })
-})
+    res.send(measurements);
+  });
+});
 
-app.listen(port, () => console.log(`Example backend API listening on port ${port}!`))
+app.post("/water", (req, res) => {
+  // https://redis.io/commands/zadd
+  let timestamp = +new Date();
+  let moistureValue = req.body.moisture;
+  let args = ["series", moistureValue, timestamp];
+  redis.zadd(args, function(err, response) {
+    if (err) throw err;
+    res.send(`${moistureValue} added`);
+  });
+});
+
+app.delete("/water", (req, res) => {
+  let key = req.body.key;
+  redis.del(key, function(err, response) {
+    if (response == 1) {
+      console.log("Deleted Successfully!");
+      res.send(`${key} deleted`);
+    } else {
+      console.log("Cannot delete");
+    }
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Example backend API listening on port ${port}!`)
+);
